ec92af9d39f6f91e2fd38dcd50e8949ce410face
alfred sang <i5ting@126.com>
2015-06-02 10:14:02 +0800

    practice with demo

--- cp.sh
+++ cp.sh
@@ -4,4 +4,4 @@
 cp -f out/app/controllers/users_controller.js ../express-g-demo/app/controllers/
 cp -f out/app/models/user.js ../express-g-demo/app/models/
 cp -f out/app/routes/users.js ../express-g-demo/app/routes/
-cp -rf out/app/views/user ../express-g-demo/app/views/
+cp -rf out/app/views/users ../express-g-demo/app/views/
