6a459903b637357cedad48e86461a77badb4f9cf
alfred sang <i5ting@126.com>
2015-07-01 23:39:29 +0800

    add moa cli

---
+++ bin/moa.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+require('shelljs/global');
+ 
+echo('Moajs HELP:');
+echo('');
+
+echo('step 0: 【修改配置】 cp config/default.example.json to config/default.json');
+echo('step 1: 【启动服务器】 npm start');
+echo('step 2: 【创建脚手架】 moag user name:string password:string uid:object');
+echo('step 3: 【如果需要，移除已有脚手架】 moad user');
+echo('Have a good day! Moaer');
