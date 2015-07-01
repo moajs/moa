bf298359228b8186b083c924dfcc756e47834129
alfred sang <i5ting@126.com>
2015-07-01 23:43:53 +0800

    refact cli

--- bin/moa.js
+++ bin/moa.js
@@ -3,9 +3,8 @@ require('shelljs/global');
  
 echo('Moajs HELP:');
 echo('');
-
-echo('step 0: 【修改配置】 cp config/default.example.json to config/default.json');
-echo('step 1: 【启动服务器】 npm start');
-echo('step 2: 【创建脚手架】 moag user name:string password:string uid:object');
-echo('step 3: 【如果需要，移除已有脚手架】 moad user');
+echo('moan: 【创建新项目】 moan new_project_name');
+echo('moag: 【创建脚手架】 moag user name:string password:string uid:object');
+echo('moad: 【移除脚手架】 moad user');
+echo('');
 echo('Have a good day! Moaer');
