7a651fd391acc0c22e3669a660c7d88a38b3f566
alfred sang <i5ting@126.com>
2015-07-02 14:17:39 +0800

    use apx/topics/ as list query api

--- tpl/routes/api/movie.js
+++ tpl/routes/api/movie.js
@@ -7,7 +7,7 @@ var $ = require('mount-controllers')(__dirname).{{models}}_controller;
 var $middlewares  = require('mount-middlewares')(__dirname);
 
 // route define
-router.get('/list', $middlewares.check_api_token, $.api.list);
+router.get('/', $middlewares.check_api_token, $.api.list);
 
 router.post('/', $middlewares.check_api_token, $.api.create);
 
