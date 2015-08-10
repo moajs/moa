187922a96643c0bdd30a8c0f08eae4bf613179d9
i5ting <i5ting@126.com>
2015-08-10 21:48:14 +0800

    refact moan with new ~/.moa dir

--- bin/moan.js
+++ bin/moan.js
@@ -2,6 +2,7 @@
 require('shelljs/global');
 
 var child_process = require('child_process');
+var fs            = require('fs');
 
 var argv = process.argv;
 argv.shift();
@@ -17,22 +18,40 @@ if(argv.length < 1){
 
 var project_name = argv[1];
 
+var home_dir = process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE;
+
 
 if (!which('git')) {
   echo('Sorry, this script requires git');
   exit(1);
 }
+var cache_dir = home_dir + '/.moa';
+var folder_exists = fs.existsSync(cache_dir);
+console.log(home_dir + ' - ' + folder_exists);
 
-var clone = 'git clone --depth=1 https://github.com/moajs/moa-seed.git ' + project_name
-// Run external tool synchronously
-if (exec(clone).code !== 0) {
-  echo('Error: Git clone failed');
-  exit(1);
+if(!folder_exists){
+  var clone = 'git clone --depth=1 https://github.com/moajs/moa-seed.git '+ cache_dir;
+  // Run external tool synchronously
+  if (exec(clone).code !== 0) {
+    echo('Error: Git clone failed');
+    exit(1);
+  }else{
+    echo('Success: exn clone finished!');
+  }
 }else{
-  echo('Success: exn clone finished!');
+  console.log(cache_dir + ' is already exist, if you want update force,please use -f option');
 }
 
-var clone_post = 'rm -rf ' + project_name + '/.git';
+var clone_cp = 'cp -rf ' + home_dir + '/.moa ' + project_name + '&& cd '+ project_name +' && npm install --save moa-plugin-user && cp config/default.example.json config/default.json';
+console.log(clone_cp)
+if (exec(clone_cp).code !== 0) {
+  echo('Error: Git clone_cp failed');
+  exit(1);
+}else{
+  echo('Success: exn clone_cp finished!');
+}
+
+var clone_post = 'rm -rf ' + project_name + '/.git && cd ' + project_name;
 if (exec(clone_post).code !== 0) {
   echo('Error: Git clone_post failed');
   exit(1);
